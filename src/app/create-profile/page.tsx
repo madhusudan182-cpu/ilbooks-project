2c78b02aae76601afd8a4d70a09f690c06eb0a9c
Madhu Sudan <madhusudan.182@gmail.com>
2026-02-04 21:08:49 +0000

    Generate a dropdown list of 64 districts' name in "District" option in C

--- src/app/create-profile/page.tsx
+++ src/app/create-profile/page.tsx
@@ -52,7 +52,77 @@ export default function CreateProfilePage() {
             <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
                <div className="grid gap-2">
                 <Label htmlFor="district">District</Label>
-                <Input id="district" placeholder="e.g., Dhaka" />
+                <Select>
+                  <SelectTrigger id="district">
+                    <SelectValue placeholder="Select your district" />
+                  </SelectTrigger>
+                  <SelectContent>
+                    <SelectItem value="bagerhat">Bagerhat</SelectItem>
+                    <SelectItem value="bandarban">Bandarban</SelectItem>
+                    <SelectItem value="barguna">Barguna</SelectItem>
+                    <SelectItem value="barishal">Barishal</SelectItem>
+                    <SelectItem value="bhola">Bhola</SelectItem>
+                    <SelectItem value="bogura">Bogura</SelectItem>
+                    <SelectItem value="brahmanbaria">Brahmanbaria</SelectItem>
+                    <SelectItem value="chandpur">Chandpur</SelectItem>
+                    <SelectItem value="chapainawabganj">Chapainawabganj</SelectItem>
+                    <SelectItem value="chattogram">Chattogram</SelectItem>
+                    <SelectItem value="chuadanga">Chuadanga</SelectItem>
+                    <SelectItem value="coxs-bazar">Cox's Bazar</SelectItem>
+                    <SelectItem value="cumilla">Cumilla</SelectItem>
+                    <SelectItem value="dhaka">Dhaka</SelectItem>
+                    <SelectItem value="dinajpur">Dinajpur</SelectItem>
+                    <SelectItem value="faridpur">Faridpur</SelectItem>
+                    <SelectItem value="feni">Feni</SelectItem>
+                    <SelectItem value="gaibandha">Gaibandha</SelectItem>
+                    <SelectItem value="gazipur">Gazipur</SelectItem>
+                    <SelectItem value="gopalganj">Gopalganj</SelectItem>
+                    <SelectItem value="habiganj">Habiganj</SelectItem>
+                    <SelectItem value="jamalpur">Jamalpur</SelectItem>
+                    <SelectItem value="jashore">Jashore</SelectItem>
+                    <SelectItem value="jhalokati">Jhalokati</SelectItem>
+                    <SelectItem value="jhenaidah">Jhenaidah</SelectItem>
+                    <SelectItem value="joypurhat">Joypurhat</SelectItem>
+                    <SelectItem value="khagrachhari">Khagrachhari</SelectItem>
+                    <SelectItem value="khulna">Khulna</SelectItem>
+                    <SelectItem value="kishoreganj">Kishoreganj</SelectItem>
+                    <SelectItem value="kurigram">Kurigram</SelectItem>
+                    <SelectItem value="kushtia">Kushtia</SelectItem>
+                    <SelectItem value="lakshmipur">Lakshmipur</SelectItem>
+                    <SelectItem value="lalmonirhat">Lalmonirhat</SelectItem>
+                    <SelectItem value="madaripur">Madaripur</SelectItem>
+                    <SelectItem value="magura">Magura</SelectItem>
+                    <SelectItem value="manikganj">Manikganj</SelectItem>
+                    <SelectItem value="meherpur">Meherpur</SelectItem>
+                    <SelectItem value="moulvibazar">Moulvibazar</SelectItem>
+                    <SelectItem value="munshiganj">Munshiganj</SelectItem>
+                    <SelectItem value="mymensingh">Mymensingh</SelectItem>
+                    <SelectItem value="naogaon">Naogaon</SelectItem>
+                    <SelectItem value="narail">Narail</SelectItem>
+                    <SelectItem value="narayanganj">Narayanganj</SelectItem>
+                    <SelectItem value="narsingdi">Narsingdi</SelectItem>
+                    <SelectItem value="natore">Natore</SelectItem>
+                    <SelectItem value="netrokona">Netrokona</SelectItem>
+                    <SelectItem value="nilphamari">Nilphamari</SelectItem>
+                    <SelectItem value="noakhali">Noakhali</SelectItem>
+                    <SelectItem value="pabna">Pabna</SelectItem>
+                    <SelectItem value="panchagarh">Panchagarh</SelectItem>
+                    <SelectItem value="patuakhali">Patuakhali</SelectItem>
+                    <SelectItem value="pirojpur">Pirojpur</SelectItem>
+                    <SelectItem value="rajbari">Rajbari</SelectItem>
+                    <SelectItem value="rajshahi">Rajshahi</SelectItem>
+                    <SelectItem value="rangamati">Rangamati</SelectItem>
+                    <SelectItem value="rangpur">Rangpur</SelectItem>
+                    <SelectItem value="satkhira">Satkhira</SelectItem>
+                    <SelectItem value="shariatpur">Shariatpur</SelectItem>
+                    <SelectItem value="sherpur">Sherpur</SelectItem>
+                    <SelectItem value="sirajganj">Sirajganj</SelectItem>
+                    <SelectItem value="sunamganj">Sunamganj</SelectItem>
+                    <SelectItem value="sylhet">Sylhet</SelectItem>
+                    <SelectItem value="tangail">Tangail</SelectItem>
+                    <SelectItem value="thakurgaon">Thakurgaon</SelectItem>
+                  </SelectContent>
+                </Select>
               </div>
                <div className="grid gap-2">
                 <Label htmlFor="thana">Thana</Label>
