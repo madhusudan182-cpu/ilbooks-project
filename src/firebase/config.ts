90fa66520be46bae499888147f2f88da881bd499
Madhu Sudan <madhusudan.182@gmail.com>
2026-02-05 18:46:50 +0000

    Try fixing this error: `Runtime FirebaseError: Firebase: Error (auth/inv

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,4 +1,9 @@
-// This file is intentionally left blank.
-// The Firebase configuration will be populated by the Firebase App Hosting backend.
-// You can find your Firebase configuration in the Firebase console.
-export const firebaseConfig = {};
+export const firebaseConfig = {
+  apiKey: "AIzaSyBODkF3a2rNei3A-pIspc-Uo2a7o_2j9yM",
+  authDomain: "ilbooks-app.firebaseapp.com",
+  projectId: "ilbooks-app",
+  storageBucket: "ilbooks-app.appspot.com",
+  messagingSenderId: "991269389274",
+  appId: "1:991269389274:web:0b001d81f1295b9d36e8b4",
+  measurementId: "G-VFE6VMS2SE"
+};
