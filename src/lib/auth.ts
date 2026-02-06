722514b60cc72621ee0bd96311e7450b2855775d
Madhu Sudan <madhusudan.182@gmail.com>
2026-02-06 10:55:56 +0000

    Switch me to Paban Bangali user.

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -4,4 +4,4 @@ import type { User } from './types';
 // To switch between an admin and user view, change the index here.
 // mockUsers[0] is an admin.
 // mockUsers[1], mockUsers[2], mockUsers[3] are regular users.
-export const currentUser: User = mockUsers[0];
+export const currentUser: User = mockUsers[4];
